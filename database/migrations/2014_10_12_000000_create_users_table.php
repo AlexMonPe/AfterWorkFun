6fa867d08399c30d33b53746ca4039823ec31823
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 17:15:12 +0200

    edit: name to nick and add steamUserName field in migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nick');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('steamUserName');
+            $table->string('steamUserName')->nullable();
             $table->boolean('isActive')->default(true);
             $table->timestamps();
         });
