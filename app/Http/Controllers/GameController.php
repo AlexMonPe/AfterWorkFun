b52658d96ab9ca1a1a7461697217c30b1766f307
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 20:10:06 +0200

    create: updateGame function in GameController

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -31,27 +31,53 @@ class GameController extends Controller
     {
         try {
             $validator = Validator::make($request->all(), [
-            'name' => 'required|string',
-        ]);
+                'name' => 'required|string',
+            ]);
 
-        if ($validator->fails()) return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            if ($validator->fails()) return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
 
-        $newGame = new Game();
+            $newGame = new Game();
 
-        $newGame->name = $request->name;
+            $newGame->name = $request->name;
 
-        $newGame->save();
+            $newGame->save();
 
-        return response()->json(["success" => "Game created -> ".$newGame->name ], Response::HTTP_CREATED);
+            return response()->json(["success" => "Game created -> " . $newGame->name], Response::HTTP_CREATED);
         } catch (\Throwable $th) {
             Log::error('Error creating game ' . $th->getMessage());
+
             return response()->json([
                 "name" => $th->getMessage(),
                 "error" => 'Error creating game '
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
 
-        
+    public function updateGame(Request $request, $id)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'string',
+            ]);
 
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+            }
+
+            $game = Game::where('id', $id)->first();
+
+            if (empty($game)) return response()->json(["error" => "Game not found"], Response::HTTP_NOT_FOUND);
+
+            if (isset($request->name)) $game->name = $request->name;
+
+            $game->save();
+
+            return response()->json(["success" => "Updated game => " . $game->name], Response::HTTP_OK);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error updating game '
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
