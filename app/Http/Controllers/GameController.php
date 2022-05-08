f74d0c081484fc6ae4f444e0d9725c3d512dfbe6
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 19:26:18 +0200

    add: getAllGames envolved in tryCatch

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -4,15 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Game;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class GameController extends Controller
 {
     public function getAllGames()
     {
-        $games = Game::get()->toArray();
+        try {
+            $games = Game::get()->toArray();
 
-        foreach ($games as $game) {
-            return $game->name;
+            foreach ($games as $game) {
+                return $game->name;
+            }
+        } catch (\Throwable $th) {
+            Log::error('Error getting all games ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error getting all games '
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+   
 }
