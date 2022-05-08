7adc595c733673c780e6f9dd51c695bc8cbf954e
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 13:40:47 +0200

    create: group routes endpoints of register,login, logout and profile

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::group([
+    'middleware' => 'jwt.auth'
+], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', [AuthController::class, 'profile']);
+});
