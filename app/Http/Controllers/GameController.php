f1fe67e7c4d3037c94454bf77a448999001b68cd
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 19:33:07 +0200

    create: createGame function in GameController

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Game;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class GameController extends Controller
@@ -26,5 +27,31 @@ class GameController extends Controller
         }
     }
 
-   
+    public function createGame(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+        ]);
+
+        if ($validator->fails()) return response()->json($validator->errors(), Response::HTTP_BAD_REQUEST);
+
+        $newGame = new Game();
+
+        $newGame->name = $request->name;
+
+        $newGame->save();
+
+        return response()->json(["success" => "Game created -> ".$newGame ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            Log::error('Error creating game ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error creating game '
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        
+
+    }
 }
