66466e486465978a60fa423215f6be859ccc96e9
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 12:35:56 +0200

    add: field isActive to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('steamUserName');
+            $table->boolean('isActive')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
