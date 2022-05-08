3b54d89b809af13e51575b58109d6e4e7cbf34a5
Alejandro <Alex_bcn10@hotmail.es>
2022-05-08 18:29:55 +0200

    edit & create: changed response code to variables of response, created logout and works

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,7 @@ class AuthController extends Controller
                 'steamUserName' => 'string|max:45',
             ]);
             if ($validator->fails()) {
-                return response()->json($validator->errors()->toJson(), 400);
+                return response()->json($validator->errors()->toJson(), Response::HTTP_BAD_REQUEST);
             }
             $user = User::create([
                 'nick' => $request->get('nick'),
@@ -31,10 +31,10 @@ class AuthController extends Controller
             ]);
             $token = JWTAuth::fromUser($user);
             Log::info('new user created called ' . $user->nick);
-            return response()->json(compact('user', 'token'), 201);
+            return response()->json(compact('user', 'token'), Response::HTTP_ACCEPTED);
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
-            return response()->json(['error' => ' error registering user ' . $th->getMessage()], 500);
+            return response()->json(['error' => ' error registering user ' . $th->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -54,10 +54,29 @@ class AuthController extends Controller
             return response()->json([
                 'success' => true,
                 'token' => $jwt_token,
-            ]);
+            ], Response::HTTP_ACCEPTED);
         } catch (\Throwable $th) {
             Log::error($th->getMessage());
-            return response()->json(['error' => ' Error in login ' . $th->getMessage()], 500);
+            return response()->json(['error' => ' Error in login ' . $th->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+        try {
+            JWTAuth::invalidate($request->token);
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
