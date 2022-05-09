414de3cc431a79d66e71d5cf858d6dd33caedd60
Alejandro <Alex_bcn10@hotmail.es>
2022-05-09 18:01:41 +0200

    add: function delete user logged parties

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -105,4 +105,24 @@ class PartyController extends Controller
             ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
+
+    public function deleteParty($id){
+        try {
+            $userId = auth()->user()->id;
+
+            $party = Party::where('id', $id)->where('user_id', $userId)->first();
+            
+            if (empty($party)) return response()->json(["error" => "Party not found"], Response::HTTP_NOT_FOUND);
+
+            $party->delete();
+
+            return response()->json(["success" => "Deleted party => " . $party->name], Response::HTTP_OK);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error deleting party '
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
