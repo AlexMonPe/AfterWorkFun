5e6da600ea2ccda0b1e7d512ad9f5cda05dd4d2f
Alejandro <Alex_bcn10@hotmail.es>
2022-05-09 19:41:04 +0200

    add: function getMessagesFromParty

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Party;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends Controller
 {
-    //
+    public function getAllMessagesFromParty($id)
+    {
+        try {
+            $messages = Party::find($id)->messages;
+
+            return $messages;
+        } catch (\Throwable $th) {
+            Log::error('Error getting all parties ' . $th->getMessage());
+            return response()->json([
+                "name" => $th->getMessage(),
+                "error" => 'Error getting all parties '
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
 }
