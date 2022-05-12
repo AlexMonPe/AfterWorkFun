9af993de7b95a386d7b1d2f4f75e804c082fd546
Alejandro <Alex_bcn10@hotmail.es>
2022-05-12 16:27:50 +0200

    create: UserRole seeder and added in databaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             PartySeeder::class,
+            UserRoleSeeder::class,
         ]);
     }
 }
